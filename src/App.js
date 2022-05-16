f962e0391b9d21ed61b132f75cd28d152fe4e083
Deennn <abideenlawal70@gmail.com>
2022-05-16 01:24:50 +0100

    cool stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
+import Navbar from "./components/layouts/Navbar";
+import UserItem from "./components/users/UserItem";
 
 import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello form react</h1>
-        </div>
-    )
+class App extends Component {
+
+    constructor () {
+        super();
+        this.state =  {
+            id: "1",
+            login: "mojombo",
+            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+            html_url: 'https://github.com/mojombo'
+        }
+    }
+    render () {
+
+        return (
+            <div className="App">
+                <Navbar/>
+                <UserItem/>
+            </div>
+        );
+    }
 }
 export default App; 
