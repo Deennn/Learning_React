608466fb52ed18f39738a5ccee0eb4499c030671
Deennn <abideenlawal70@gmail.com>
2022-05-16 08:41:10 +0100

    it's getting interesting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, {Component} from "react";
 import Navbar from "./components/layouts/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 import './App.css'
 
 class App extends Component {
 
-    constructor () {
-        super();
-        this.state =  {
-            id: "1",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: 'https://github.com/mojombo'
-        }
-    }
+   
     render () {
 
         return (
             <div className="App">
                 <Navbar/>
-                <UserItem/>
+            <div className="container">
+            <Users/>
+            </div>
             </div>
         );
     }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
+const UserItem = ({user: {login, avatar_url, html_url} })=> {
 
-    constructor () {
-        super();
-        this.state =  {
-            id: "1",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: 'https://github.com/mojombo'
-        }
-    }
-
-  render() {
-
-    const {login, avatar_url, html_url} = this.state;
     return (
       <div className='card text-center'>
         <img src={avatar_url} alt="" className="round-img"
         style={{width: '200px'}}/>
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
 
         <div>
             <a href={html_url} className='btn btn-dark btn-sm-my-1'> </a>
         </div>
       </div>
     )
-  }
+ 
 }
 
 export default UserItem
